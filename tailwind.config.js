47ec567e830c14c9d4afebc7eee02150aa47697b
Rafael Gilete <rafaelgiletemogena@gmail.com>
2024-01-24 20:54:25 +0100

    astro migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html"],
-  daisyui: {
-    themes: ["dark"],
-  },
-  plugins: [require("daisyui")],
-}
+export default {
+    content: ['./src/**/*.{astro,html,jsx,mdx,tsx}'],
+    plugins: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: 'Poppins, ui-sans-serif, system-ui',
+            },
+        },
+    },
+};
